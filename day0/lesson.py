b9fac4e79164a85c8a3440f4e0d9d949cfeae0f8
mariambedoidze04 <mari4bedoidze@gmail.com>
2024-05-03 00:15:53 +0400

    first commit

---
+++ day0/lesson.py
@@ -0,0 +1,6 @@
+name = "Mariam"
+surname = "Bedoidze"
+age = 21
+height = 171.5
+
+print("my name is"+" "+name+ "."+" "+"I am"+" " + str(age)+" "+"years old."+" "+"I'm"+" "+str(height)+" "+"centimeters tall.")
